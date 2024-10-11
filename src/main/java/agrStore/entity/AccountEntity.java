f1fefc95e1d3f95d5089e67cd7e5f7a3434f4a24
dinhquoctoan <dinhquoctoan200103@gmail.com>
2024-10-11 16:25:44 +0700

    sửa lỗi nhanh

--- src/main/java/agrStore/entity/AccountEntity.java
+++ src/main/java/agrStore/entity/AccountEntity.java
@@ -63,18 +63,17 @@ public class AccountEntity {
 	@JoinColumn(name = "addressId")
 	private AddressEntity address;
 	
-	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
-	private CustomerEntity customer;
+//	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
+//	private CustomerEntity customer;
 	
-	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
-	private StaffEntity staff;
+//	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
+//	private StaffEntity staff;
 	
-	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
-	private AdminEntity admin;
+//	@OneToOne(mappedBy = "account", fetch = FetchType.LAZY)
+//	private AdminEntity admin;
 
 	public AccountEntity(Integer id, Boolean status, String avatar, String gmail, String fullName, String phoneNumber,
-			String password, Date createAt, Date updateAt, RoleEntity role, AddressEntity address,
-			CustomerEntity customer, StaffEntity staff, AdminEntity admin) {
+			String password, Date createAt, Date updateAt, RoleEntity role, AddressEntity address) {
 		super();
 		this.id = id;
 		this.status = status;
@@ -87,9 +86,6 @@ public class AccountEntity {
 		this.updateAt = updateAt;
 		this.role = role;
 		this.address = address;
-		this.customer = customer;
-		this.staff = staff;
-		this.admin = admin;
 	}
 
 	public AccountEntity() {
@@ -195,30 +191,6 @@ public class AccountEntity {
 
 	public void setAddress(AddressEntity address) {
 		this.address = address;
-	}
-
-	public CustomerEntity getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(CustomerEntity customer) {
-		this.customer = customer;
-	}
-
-	public StaffEntity getStaff() {
-		return staff;
-	}
-
-	public void setStaff(StaffEntity staff) {
-		this.staff = staff;
-	}
-
-	public AdminEntity getAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(AdminEntity admin) {
-		this.admin = admin;
 	}
 	
 }
